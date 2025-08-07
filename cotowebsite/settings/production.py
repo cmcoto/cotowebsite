7304108c8525893d86d81ef1d6c3908c49ff9e16
cmcoto <cmcoto@gmail.com>
2025-08-07 17:56:21 +0200

    Setting up for production

--- cotowebsite/settings/production.py
+++ cotowebsite/settings/production.py
@@ -2,6 +2,15 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cmcoto$default',
+        'USER': 'cmcoto',
+        'PASSWORD': 'pacesetter',
+        'HOST': 'cmcoto.mysql.pythonanywhere-services.com',
+    }
+}
 
 try:
     from .local import *
